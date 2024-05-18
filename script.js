97259b2aeb7aa52e13018ad0355cdc1075fd7141
Vojislav Grujic <vojislav.grujic@gmail.com>
2024-05-18 11:01:49 +0200

    Add trader and strategy select fields

--- script.js
+++ script.js
@@ -1,10 +1,52 @@
-window.addEventListener('load', runScript);
-
-async function runScript() {
-  const strategiesResponse = await fetch('./strategies.json')
+window.addEventListener('load', async () => {
+  const strategyDocument = await fetch('./strategies.json')
     .then(response => response.json()).then(json => json);
 
-  console.log({
-    strategiesResponse
-  })
-}
+  const traderSelect = document.getElementById('trader-selection');
+  const strategySelect = document.getElementById('strategy-selection');
+
+  let traders = [];
+  let strategies = [];
+
+  const createOption = (value) => {
+    const option = document.createElement('option');
+    option.value = value;
+    option.innerText = value;
+    return option;
+  };
+
+  const populateSelectField = (values, parent) => {
+    values.forEach(value => parent.appendChild(createOption(value)));
+  };
+  traderSelect.addEventListener('change', updateTrader);
+
+  traders = strategyDocument.map(trader => trader.traderName);
+  populateSelectField(traders, traderSelect);
+
+  function updateTrader(e) {
+    const traderName = e.target.value;
+
+    if (traderName === 'none') {
+      // TODO: Handle this with clearing the strategy selection and page display
+      return;
+    }
+
+    const strategies = strategyDocument.filter(trader => trader.traderName === traderName)?.[0]?.strategies;
+    const strategyNames = strategies.map(strategy => strategy.strategyName);
+    populateSelectField(strategyNames, strategySelect);
+    strategySelect.addEventListener('change', updateStrategy);
+
+    // TODO: Decouple from updateTrader if possible
+    function updateStrategy(e) {
+      const strategyName = e.target.value;
+
+      if (strategyName === 'none') {
+        // TODO: Handle this with clearing the strategy selection and page display
+        return;
+      }
+
+      const selectedStrategy = strategies.filter((strategy) => strategy.strategyName === strategyName);
+      console.log(selectedStrategy[0]);
+    }
+  }
+});
